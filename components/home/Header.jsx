15dd03fa2465b1ce2203823ad183ab0dc5778d37
Nabeel Boda <needtoexplore01@gmail.com>
2024-02-13 01:29:09 +0530

    Feat: Sticky Header

--- components/home/Header.jsx
+++ components/home/Header.jsx
@@ -2,7 +2,7 @@ import SecondaryBtn from "../SecondaryBtn";
 
 export default function Header() {
   return (
-    <header className="flex justify-between pt-5 items-center">
+    <header className="flex justify-between pt-5 items-center sticky top-0 py-5 z-20">
       <h1 className="text-3xl font-medium text-[#333333]">Indic Subtitler</h1>
       <SecondaryBtn />
     </header>
